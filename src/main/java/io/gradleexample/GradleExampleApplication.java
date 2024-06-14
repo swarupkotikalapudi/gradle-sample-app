1aa2b697ef2be8ff8be3720990690bea009ec5b6
swarupkotikalapudi <swarupkotikalapudi@gmail.com>
2024-06-14 15:13:32 +0530

    Update GradleExampleApplication.java in Demo Branch

--- src/main/java/io/gradleexample/GradleExampleApplication.java
+++ src/main/java/io/gradleexample/GradleExampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GradleExampleApplication {
 
 	public static void main(String[] args) {
+		// Demo branch..
 		SpringApplication.run(GradleExampleApplication.class, args);
 	}
 }
