a68f8170b84f955d498c92b93ff06d6f3f4888e5
swarupkotikalapudi <swarupkotikalapudi@gmail.com>
2024-06-18 22:07:37 +0530

    Update GradleExampleApplication.java

--- src/main/java/io/gradleexample/GradleExampleApplication.java
+++ src/main/java/io/gradleexample/GradleExampleApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GradleExampleApplication {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		SpringApplication.run(GradleExampleApplication.class, args);
 	}
 }
